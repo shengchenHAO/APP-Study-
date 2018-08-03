9631e3a0338c8c04305be8d5759a45a90f1f22d8
shengchenHAO <shhao@umich.edu>
2018-08-03 01:19:39 -0400

    separting no shared visit competing risk model into single job for flux

--- Untitled.R
+++ Untitled.R
@@ -1,5 +1,5 @@
 library(cmprsk2)
-load("competing risk.RData")
+load("data_competing_risk.RData")
 output1 <- crr2(Surv(time, Status(1)== 2) ~ Age+ Sex+ score + AC+ Hepatitis_C+ Non_alcohol+ Ascites+ Varices+ HE+ HCC+ APP+Gastro_only+Hepatology+SBP+TIPS+Race+APP_Gastro+APP_Hepatology+Pneumonia+Sepsis+Urinary_tract_infection+Cellulitis+Bacteremia+Clostridium+Cholangitis+Paracentesis+Dialysis+max_visit+Shared_visit,
                 data = table2)
 
