4049dc3adcaee43519d4aad3894023c4b2a2a17e
ShengchenHao <shhao@umich.edu>
2018-08-03 23:28:34 -0400

    edit

--- New Table.R
+++ New Table.R
@@ -55,6 +55,7 @@ apply(Count_table1, 2, sum)
 
 
 # time senstive version 
+HCC_patid = c()
 SBRT_patid = c()
 TACE_patid = c() 
 Total_ablation_patid = c() 
@@ -67,13 +68,14 @@ for (year in 2001:2015){
   name = paste0("data_def_", year, ".RData") 
   load(name) 
   
+  HCC_patid = c(HCC_patid, unique(filter(temp_data, HCC == 1)$Patid))
   SBRT_patid = c(SBRT_patid, unique(filter(temp_data, SBRT == 1 & HCC == 1)$Patid))
   TACE_patid = c(TACE_patid, unique(filter(temp_data, TACE == 1 & HCC == 1)$Patid)) 
   Total_ablation_patid = c(Total_ablation_patid, unique(filter(temp_data, (Ablation ==1 | RFA == 1 | Cryo == 1) & HCC == 1 )$Patid))
   RFA_patid = c(RFA_patid, unique(filter(temp_data, RFA == 1 & HCC == 1)$Patid))
   Cryo_patid = c(Cryo_patid, unique(filter(temp_data, Cryo == 1 & HCC == 1)$Patid)) 
   Hepatectomy_patid = c(Hepatectomy_patid, unique(filter(temp_data, (grepl("47120", Proc) & Fst_Dt < Lst_Date & HCC == 1) | (partial_hep == 1 & HCC == 1))$Patid))
-  liver_transplant_patid = c(liver_transplant_patid, unique(filter(temp_data, (grepl("^505", Diag)|grepl("47135|47136", Proc)) & HCC == 1)$Patid))
+  liver_transplant_patid = c(liver_transplant_patid, unique(filter(temp_data, (grepl("^505", Diag)|grepl("47135|47136", Proc)) & Fst_Dt > Cancer_Diag_Date)$Patid))
   
   rm(temp_data, year, name) 
   gc()
