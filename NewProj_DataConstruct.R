adc435aa574af2d41ef6eb6e9396e2806285b340
shengchenHAO <shhao@umich.edu>
2018-08-08 16:44:55 -0400

    New proj, excluding preparing, first GI date

---
+++ NewProj_DataConstruct.R
@@ -0,0 +1,25 @@
+setwd("X:/Shengchen Hao/Tapper Liver/R file/new") 
+library(haven)  
+library(dplyr) 
+library(stringr) 
+library(tidyr) 
+
+
+# FIRST EXCLUDE BASED ON CODE BOOK 
+for (year in 2001:2015){
+  name = paste0("data_censor_", year, ".RData") 
+  load(name) 
+  
+  GI_visit = rbind(GI_visit, filter(temp_data, Gastro == 1))
+  
+  rm(temp_data,name,year)
+  gc()
+}
+
+GI_first_date = GI_visit %>% 
+  select(Patid, Fst_Dt) %>% 
+  distinct() %>%
+  arrange(Patid, Fst_Dt) %>% 
+  group_by(Patid) %>% 
+  summarise(GI_first_date = first(Fst_Dt))
+
