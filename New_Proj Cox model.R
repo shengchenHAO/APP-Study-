b629c6f4cac7d906ca0e8308c9e619bb530363e5
ShengchenHao <shhao@umich.edu>
2018-08-23 14:31:38 -0400

    cox model

---
+++ New_Proj Cox model.R
@@ -0,0 +1,17 @@
+setwd("X:/Shengchen Hao/Tapper Liver/R file/new/GI_proj") 
+library(haven)  
+library(dplyr) 
+library(stringr) 
+library(tidyr) 
+library(survival)
+library(survminer)
+
+load("Table1_complete.RData")
+
+
+
+
+
+
+
+
